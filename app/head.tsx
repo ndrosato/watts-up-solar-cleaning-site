0da80e67faaf8e1c7234d8cd06682336b13a9b11
ndrosato <ndrosato@gmail.com>
2025-04-04 09:31:46 +1100

    Manually inject favicon in app/head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+      <>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </>
+    )
+  }
+  
