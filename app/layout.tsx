0f7565cccd05d0ea9e08bdca66fe793052dc6619
ndrosato <ndrosato@gmail.com>
2025-04-04 09:14:33 +1100

    include favicon link

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="google-site-verification" content="YOUR_VERIFICATION_CODE" />
       </head>
       <body>
